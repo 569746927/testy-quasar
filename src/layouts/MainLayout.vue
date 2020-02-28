4966ae17213535e5c6a248f39e81f26da11ed395
Linqiang <569746927@qq.com>
2020-02-28 11:42:16 +0800

    update drawer to expanditem

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -80,12 +80,21 @@
         </q-card>
       </q-expansion-item>
 
-      <q-expansion-item icon="assessment" label="Disabled" disable>
+      <q-expansion-item icon="settings" label="平台管理" >
         <q-card>
           <q-card-section>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
-            commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
-            eveniet doloribus ullam aliquid.
+            <q-item
+              clickable
+              v-ripple
+              to="/manage"
+              active-class="my-menu-link"
+            >
+              <q-item-section avatar>
+                <q-icon name="supervised_user_circle" />
+              </q-item-section>
+
+              <q-item-section>用户管理</q-item-section>
+            </q-item>
           </q-card-section>
         </q-card>
       </q-expansion-item>
@@ -109,3 +118,8 @@ export default {
   }
 }
 </script>
+<style lang="sass" scoped>
+.my-menu-link
+  color: white
+  background: #F2C037
+</style>
